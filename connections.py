f2f4a9d08817f7567ca1e99a1893e4050756e721
Ritesh <choudhary.rit@northeastern.edu>
2024-04-23 12:37:06 -0400

    Update connections.py

--- connections.py
+++ connections.py
@@ -1,11 +1,11 @@
-import configparser
-config = configparser.ConfigParser()
-config.read('configuration.properties')
+
+import os
+
 
 def pinecone_connection():
     try:
-        pinecone_api_key = config['PINECONE']['PINECONE_API_KEY']
-        index_name = config['PINECONE']['INDEX']
+        pinecone_api_key = os.getenv('PINECONE_API_KEY')
+        index_name =  os.getenv('INDEX')
         return pinecone_api_key, index_name
     except Exception as e:
         print("Exception in pinecone_connection function: ", e)
@@ -14,7 +14,7 @@ def pinecone_connection():
 
 def openai_connection():
     try:
-        openai_api_key = config['OPENAI']['OPENAI_API_KEY']
+        openai_api_key = os.getenv('OPENAI_API_KEY')
         return openai_api_key
     except Exception as e:
         print("Exception in openai_connection function: ", e)
